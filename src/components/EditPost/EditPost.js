3f865f6e573c9b4bfec275c8afd97503c5cf45af
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-27 14:01:00 +0600

    Realized created post sending

--- src/components/EditPost/EditPost.js
+++ src/components/EditPost/EditPost.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react';
+import axiosApi from "../../axiosApi";
+
+import Spinner from "../UI/Spinner/Spinner";
 import './EditPost.css';
 
 const EditPost = () => {
@@ -6,6 +9,10 @@ const EditPost = () => {
         title: '',
         text: '',
     });
+
+    const [loading, setLoading] = useState(false);
+
+    const [fieldError, setFieldError] = useState('');
 
     const handleFieldChange = e => {
         const {name, value} = e.target;
@@ -15,35 +22,64 @@ const EditPost = () => {
         }));
     };
 
-    const createPost = e => {
+    const sendCreatePostRequest = async () => {
+        await axiosApi.post('/posts.json', {
+            title: post.title,
+            text: post.text
+        });
+    };
+
+    const createPost = async e => {
         e.preventDefault();
-        console.log(post);
+        setLoading(true);
+
+        try {
+            if (post.title && post.text) {
+                await sendCreatePostRequest();
+
+                setPost({
+                    title: '',
+                    text: '',
+                });
+
+                setFieldError('');
+            } else {
+                setFieldError('* Fill all the fields, please!');
+            }
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
         <div className="EditPostForm">
-            <form onSubmit={createPost}>
-                <label>
-                    <p>Title</p>
-                    <input
-                        type="text"
-                        name="title"
-                        value={post.title}
-                        onChange={handleFieldChange}
-                    />
-                </label>
-                <label>
-                    <p>Description</p>
-                    <textarea
-                        name="text"
-                        value={post.text}
-                        onChange={handleFieldChange}
-                    />
-                </label>
-                <button type="submit">Save</button>
-            </form>
+            {loading
+                ?
+                <Spinner />
+                :
+                <form onSubmit={createPost}>
+                    <label>
+                        <p>Title</p>
+                        <input
+                            type="text"
+                            name="title"
+                            value={post.title}
+                            onChange={handleFieldChange}
+                        />
+                    </label>
+                    <label>
+                        <p>Description</p>
+                        <textarea
+                            name="text"
+                            value={post.text}
+                            onChange={handleFieldChange}
+                        />
+                    </label>
+                    <button type="submit">Save</button>
+                </form>
+            }
+            <p className="FieldError">{fieldError}</p>
         </div>
-
     );
 };
 
