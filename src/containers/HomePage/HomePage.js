9d89ee34314e005b31b134b89d5373f39639e780
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-27 15:59:28 +0600

    Realized realized getting posts

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axiosApi from "../../axiosApi";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+
+            try {
+                const posts = await getPosts();
+                setPosts(posts);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, []);
+
+    const getPosts = async () => {
+        const response = await axiosApi.get('/posts.json');
+        return response.data;
+    };
+
     return (
         <div>
             Home
