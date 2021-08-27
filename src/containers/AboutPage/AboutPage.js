ac3068b7f86e4a390bac369fd267731772b27193
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-08-27 23:17:26 +0600

    Added data to About and Contacts pages

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './AboutPage.css';
 
 const AboutPage = () => {
     return (
-        <div>
-            About
+        <div className="Container">
+            <div className="AboutPage">
+                <h3>Hello! This is my personal blog.</h3>
+                <h4>Something about me:</h4>
+                <p>My first name is <b>Irina</b></p>
+                <p>My last name is <b>Isakzhanova</b></p>
+                <p>My hobby is dancing, watching good movies and painting</p>
+                <p>My main occupation is programming, but I spend my spare time writing interesting posts in this blog</p>
+            </div>
         </div>
+
     );
 };
 
--- src/containers/ContactsPage/ContactsPage.js
+++ src/containers/ContactsPage/ContactsPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './ContactsPage.css';
 
 const ContactsPage = () => {
     return (
-        <div>
-            Contacts
+        <div className="Container">
+            <div className="ContactsPage">
+                <h3>Contacts:</h3>
+                <p>Phone: 0777 20 88 42</p>
+                <p>Email: irina.isakjanova@gmail.com</p>
+                <p>Instagram: @irina_isakjanova</p>
+            </div>
+
         </div>
     );
 };
